ad8aa7b7584883f75e1e21acc5e00244ff85fc28
yurij <stiker757@mail.ru>
2022-06-03 15:06:16 +0300

    Отредактировал коментрии
    Сдедал переносы, чтобы мои комментарии отличались от комментриев ученика

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 import datetime as dt
 
-
-class Record: # Нет докстрингов к классам и функциям
+# Нет докстрингов к классам и функциям
+class Record: 
     def __init__(self, amount, comment, date=''):
         self.amount = amount
         self.date = (
-            dt.datetime.now().date() if # Не верно написаны переносы для if
+            # Не верно написаны переносы для if
+            dt.datetime.now().date() if 
             not
             date else dt.datetime.strptime(date, '%d.%m.%Y').date())
         self.comment = comment
@@ -15,12 +16,15 @@ class Calculator:
     def __init__(self, limit):
         self.limit = limit
         self.records = []
-
+    # Желательно писать тайпхинтинги
+    # Т.Е. типы того что ты передаёшь напримерн (record: str)
     def add_record(self, record):
         self.records.append(record)
 
     def get_today_stats(self):
         today_stats = 0
+        # Использовать переменные цикла с маленькой буквы
+        # с большой буквы используются только названия классов
         for Record in self.records:
             if Record.date == dt.datetime.now().date():
                 today_stats = today_stats + Record.amount
@@ -39,12 +43,15 @@ class Calculator:
 
 
 class CaloriesCalculator(Calculator):
+    # Надо использовать докстринги для описания действия функций
     def get_calories_remained(self):  # Получает остаток калорий на сегодня
         x = self.limit - self.get_today_stats()
-        if x > 0: # Нельзя использовать бэкслэши в f строках
+        if x > 0:
+            # Нельзя использовать бэкслэши в f строках 
             return f'Сегодня можно съесть что-нибудь' \
                    f' ещё, но с общей калорийностью не более {x} кКал'
-        else:# Можно не писать else а просто return в функции
+        # Можно не писать else а просто вернуть в return самой функции
+        else:
             return('Хватит есть!')
 
 
@@ -56,6 +63,8 @@ class CashCalculator(Calculator):
                                 USD_RATE=USD_RATE, EURO_RATE=EURO_RATE):
         currency_type = currency
         cash_remained = self.limit - self.get_today_stats()
+        # Попробовать поменять множественные if на более понятную конструкцию.
+        # Не надо использование то currency_type то currency
         if currency == 'usd':
             cash_remained /= USD_RATE
             currency_type = 'USD'
@@ -64,21 +73,25 @@ class CashCalculator(Calculator):
             currency_type = 'Euro'
         elif currency_type == 'rub':
             cash_remained == 1.00
+            # Использовать только один язык для обызначений
             currency_type = 'руб'
         if cash_remained > 0:
-            return ( # Нельзя использовать операции в f строках 
+            return (
+                # Нельзя использовать операции в f строках  
                 f'На сегодня осталось {round(cash_remained, 2)} '
                 f'{currency_type}'
             )
         elif cash_remained == 0:
             return 'Денег нет, держись'
-        elif cash_remained < 0:# Не использовать бэкслэши в f строчках
+        elif cash_remained < 0:
+            # Не использовать бэкслэши в f строчках
             return 'Денег нет, держись:' \
                    ' твой долг - {0:.2f} {1}'.format(-cash_remained,
                                                      currency_type)
-
-    def get_week_stats(self): # Можно не переопределять он автоматически наследуется
-        super().get_week_stats() # Если не переопределять то будет работать без super
+    # Можно не переопределять он автоматически наследуется
+    def get_week_stats(self):
+        # Если не переопределять то будет работать без super 
+        super().get_week_stats() 
 
 
 # Нет  if __name__ == "__main__":
